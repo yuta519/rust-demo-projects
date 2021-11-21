b39bf87939ff87c35afbdc2f0258f54a7ea11016
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-11-21 22:57:19 +0900

    add stdin treats

--- shell-demo/src/main.rs
+++ shell-demo/src/main.rs
@@ -1,4 +1,11 @@
-fn main() {
-    println!("Hello, world!");
-    println!("This is print test by yuta519");
+use std::io;
+
+fn main() -> io::Result<()> {
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut buffer)?;
+    Ok(())
+
+    // println!("Hello, world!");
+    // println!("This is print test by yuta519");
 }
