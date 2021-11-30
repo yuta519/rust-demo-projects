fcb6da52db993f8f260138a62cc122a990f4f8cf
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-11-30 11:50:55 +0900

    implment path operation to identify whether / or not

--- rust-server/src/main.rs
+++ rust-server/src/main.rs
@@ -16,16 +16,25 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
+    let get = b"GET / HTTP/1.1";
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
-    let mut file = File::open("hello.html").unwrap();
-
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    if buffer.starts_with(get) {
+        let mut file = File::open("hello.html").unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        let mut file = File::open("404.html").unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+        let response = format!("{}{}", status_line, contents);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
 
-// Please execute `curl localhost:8080`
+// Please execute `curl localhost:8080/`
