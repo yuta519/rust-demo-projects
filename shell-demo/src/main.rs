d81913b5ed7ae408403e210a769ecf67a4236256
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-12-02 11:53:41 +0900

    add secret_number

--- shell-demo/src/main.rs
+++ shell-demo/src/main.rs
@@ -1,13 +1,18 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("This is a stdin test! Guess the number.");
     println!("Please input your guessed number.");
 
+    let secret_number = rand::thread_rng().gen_range(1, 101);
     let mut buffer = String::new();
     let stdin = io::stdin();
 
-    stdin.read_line(&mut buffer)
+    stdin
+        .read_line(&mut buffer)
         .expect("Input error: read_line()");
+
     println!("Your input is {}", buffer);
+    println!("Secret is {}", secret_number);
 }
