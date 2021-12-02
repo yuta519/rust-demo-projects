3a1530b3a36493d74426199e4d721597bb6324ef
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-12-02 11:28:02 +0900

    improve a little

--- shell-demo/src/main.rs
+++ shell-demo/src/main.rs
@@ -1,11 +1,13 @@
 use std::io;
 
 fn main() {
-    println!("This is a stdin test! Please input some words or sentences.");
+    println!("This is a stdin test! Guess the number.");
+    println!("Please input your guessed number.");
+
     let mut buffer = String::new();
     let stdin = io::stdin();
-    stdin
-        .read_line(&mut buffer)
+
+    stdin.read_line(&mut buffer)
         .expect("Input error: read_line()");
-    println!("Your input is `{}`", buffer);
+    println!("Your input is {}", buffer);
 }
