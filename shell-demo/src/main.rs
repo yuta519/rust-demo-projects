2fb4d80757fc22bf4c69721a9dd9da5be2aec53a
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-12-04 23:37:37 +0900

    add loop

--- shell-demo/src/main.rs
+++ shell-demo/src/main.rs
@@ -8,20 +8,22 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
     let mut buffer = String::new();
-    let stdin = io::stdin();
 
-    stdin
-        .read_line(&mut buffer)
-        .expect("Input error: read_line()");
+    loop {
+        let stdin = io::stdin();
+        stdin
+            .read_line(&mut buffer)
+            .expect("Input error: read_line()");
 
-    let buffer: u32 = buffer.trim().parse().expect("Please input a number");
+        let guess: u32 = buffer.trim().parse().expect("Please input a number");
 
-    println!("Your input is {}", buffer);
-    println!("Secret is {}", secret_number);
+        println!("Your input is {}", guess);
+        println!("Secret is {}", secret_number);
 
-    match buffer.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!!"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!!"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => println!("You win!"),
+        }
     }
 }
