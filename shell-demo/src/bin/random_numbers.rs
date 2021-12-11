d3d1ee08125484d299f2ebaf87e1bb26d1a11391
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-12-11 23:55:44 +0900

    change random numbers

--- shell-demo/src/bin/random_numbers.rs
+++ shell-demo/src/bin/random_numbers.rs
@@ -2,6 +2,12 @@ use rand::Rng;
 
 fn main() {
     let mut rng = rand::thread_rng();
-    let random_int: u32 = rng.gen_range(0, 10);
-    println!("{}", random_int);
+
+    let n1: u8 = rng.gen();
+    let n2: u16 = rng.gen();
+    println!("Random u8: {}", n1);
+    println!("Random u16: {}", n2);
+    println!("Random u32: {}", rng.gen::<u32>());
+    println!("Random i32: {}", rng.gen::<i32>());
+    println!("Random float: {}", rng.gen::<f64>());
 }
