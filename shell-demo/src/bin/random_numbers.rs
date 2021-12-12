526c996bcd59f16214ac547c647c64ef80b93722
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-12-12 18:33:28 +0900

    add scope of random range

--- shell-demo/src/bin/random_numbers.rs
+++ shell-demo/src/bin/random_numbers.rs
@@ -5,9 +5,14 @@ fn main() {
 
     let n1: u8 = rng.gen();
     let n2: u16 = rng.gen();
+
     println!("Random u8: {}", n1);
+    println!("Random u8: {}", rng.gen::<u8>());
     println!("Random u16: {}", n2);
     println!("Random u32: {}", rng.gen::<u32>());
     println!("Random i32: {}", rng.gen::<i32>());
     println!("Random float: {}", rng.gen::<f64>());
+
+    println!("Integer Random: {}", rng.gen_range(0, 10));
+    println!("Float Random: {}", rng.gen_range(0.0, 10.0));
 }
