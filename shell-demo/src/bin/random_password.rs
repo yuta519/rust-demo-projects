671e3f063a3533ab704e45d485ec0561ee839373
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-12-13 23:25:56 +0900

    add new file for password generator

---
+++ shell-demo/src/bin/random_password.rs
@@ -0,0 +1,12 @@
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+
+fn main() {
+    let rand_string: String = thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(30)
+        .map(char::from)
+        .collect();
+
+    println!("{}", rand_string);
+}
