2b2848262aa898c7de67be625442c72f0bc198f8
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-12-14 23:42:08 +0900

    chore

--- shell-demo/src/bin/random_numbers.rs
+++ shell-demo/src/bin/random_numbers.rs
@@ -7,8 +7,8 @@ fn main() {
     let n2: u16 = rng.gen();
 
     println!("Random u8: {}", n1);
-    println!("Random u8: {}", rng.gen::<u8>());
     println!("Random u16: {}", n2);
+    println!("Random u8: {}", rng.gen::<u8>());
     println!("Random u32: {}", rng.gen::<u32>());
     println!("Random i32: {}", rng.gen::<i32>());
     println!("Random float: {}", rng.gen::<f64>());
