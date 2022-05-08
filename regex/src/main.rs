c491e40b4e129510714451c3b0f9a735a8d0b5ed
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-05-09 08:29:16 +0900

    add regex unittest

--- regex/src/main.rs
+++ regex/src/main.rs
@@ -6,7 +6,14 @@ fn main() {
     check_date("2022222222222-01-01");
 }
 
-fn check_date(input: &str) {
+fn check_date(input: &str) -> bool {
     let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
     print!("The format of `{}` is {}\n", input, re.is_match(input));
+    return re.is_match(input);
+}
+
+#[test]
+fn test_regex_dateformat() {
+    assert_eq!(check_date("2022-01-01"), true);
+    assert_eq!(check_date("20222222222222-01-01"), false);
 }
