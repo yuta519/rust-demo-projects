42b37c80f29d6a97bcd0c8873d84ecfc7f57fbb7
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-03-20 14:42:06 -0700

    Learn formatter command

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello, world!");
+
+    // By run the command `cargo fix --allow-dirty`, the following code will be fixed to the above code.
+    // After: for _i in 0..10 {
+    for i in 0..10 {
+        // println!("Hello, world! {}", i);
+        println!("Hello, world!");
+    }
 }
