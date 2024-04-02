44a1b2b3fe2edfbe99453d8c1f7d9de42d06a4bb
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-04-02 10:58:08 -0700

    Logging

--- my-todo/src/main.rs
+++ my-todo/src/main.rs
@@ -1,9 +1,15 @@
 use axum::{routing::get, Router};
+use std::env;
 
 #[tokio::main]
 async fn main() {
+    let log_level = env::var("RUST_LOG").unwrap_or_else(|_| "info".to_string());
+    env::set_var("RUST_LOG", &log_level);
+    tracing_subscriber::fmt::init();
+
     let app = Router::new().route("/", get(root));
     let address = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    tracing::debug!("Listening on: {}", address.local_addr().unwrap());
 
     axum::serve(address, app).await.unwrap();
 }
