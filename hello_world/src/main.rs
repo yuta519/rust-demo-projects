cd83063ffe234a0384caa85af02159733fc2153f
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-04-02 09:17:54 -0700

    Try test

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,7 +1,22 @@
 mod square;
 use square::Area;
 
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
+}
+
 fn main() {
     let squarere = square::Square::new(10);
     print!("{}", squarere.area());
+    print!("\n{}", add(1, 2))
 }
