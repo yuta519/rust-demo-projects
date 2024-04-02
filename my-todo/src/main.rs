0d8a08d6d2f26f027d20da83762e3e7424590fd9
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-04-02 10:50:39 -0700

    Create first endpoint, root

--- my-todo/src/main.rs
+++ my-todo/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+    let address = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(address, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
 }
