e046c8162e872f70871f653a716ee05e13a4cf37
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-04-20 12:39:30 -0700

    Define Todo struct and related traits

--- my-todo/src/repositories.rs
+++ my-todo/src/repositories.rs
@@ -5,3 +5,47 @@ use std::{
 
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
+
+#[derive(Debug, Error)]
+enum RepositoryError {
+    #[error("Not found: id is {0}")]
+    NotFound(i32),
+}
+
+pub trait TodoRepository: Clone + std::marker::Send + std::marker::Sync + 'static {
+    fn create(&self, payload: CreateTodo) -> ToDo;
+    fn find(&self, id: i32) -> Option<ToDo>;
+    fn all(&self) -> Vec<ToDo>;
+    fn update(&self, id: i32, payload: UpdateTodo) -> anyhow::Result<ToDo>;
+    fn delete(&self, id: i32) -> anyhow::Result<()>;
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+pub struct ToDo {
+    id: i32,
+    text: String,
+    completed: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+pub struct CreateTodo {
+    text: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+pub struct UpdateTodo {
+    text: Option<String>,
+    completed: Option<bool>,
+}
+
+impl Todo {
+    pub fn new(id: i32, text: String) -> Self {
+        Self {
+            id,
+            text,
+            completed: false,
+        }
+    }
+}
+
+type ToDoDataset = HashMap<i32, ToDo>;
